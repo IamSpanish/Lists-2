79f8f1779ef052e08d2210491d1bd4f3685c583d
IamSpanish <>
2021-11-11 09:15:32 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,4 @@
+import * as promptSync from 'prompt-sync' ;
+const prompt = promptSync ()
+
+const simon_says = ["Hands on head", "Hands on ears", "Right hand up", "Left hand up", "Hands on shoulders"]
